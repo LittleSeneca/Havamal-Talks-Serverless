d7e79da7c1ba3c52b19eff4cdc02482a7cfd0423
Graham Brooks <graham.brooks@nativecharter.com>
2024-01-30 12:22:19 -0800

    better cleanup process

--- cleanup.sh
+++ cleanup.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 
-# Install AWS CLI
-pip install awscli --upgrade --user
-
 # Add AWS CLI executable to the PATH
 export PATH=$PATH:$HOME/.local/bin
 
 # Configure AWS CLI with your secrets
-aws configure set aws_access_key_id ${{ secrets.AWS_ACCESS_KEY_ID }}
-aws configure set aws_secret_access_key ${{ secrets.AWS_SECRET_ACCESS_KEY }}
+aws configure set aws_access_key_id ${{ AWS_ACCESS_KEY_ID }}
+aws configure set aws_secret_access_key ${{ AWS_SECRET_ACCESS_KEY }}
 aws configure set default.region us-east-1
 
 # Specify your S3 bucket name and CloudFormation stack prefix
-S3_BUCKET_NAME="havamal-talks-serverless"
+S3_BUCKET_NAME="${{ S3_BUCKET_NAME }}"
 STACK_PREFIX="havamal-talks-serverless"
 
 # Step 1: Remove all files from the S3 bucket
