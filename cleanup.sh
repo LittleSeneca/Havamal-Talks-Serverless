8eb4f72293ab02f39dfeed1e50af35c8cf8d90f2
Graham Brooks <graham.brooks@nativecharter.com>
2024-01-30 12:12:52 -0800

    Cleanup script added

--- cleanup.sh
+++ cleanup.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+# Install AWS CLI
+pip install awscli --upgrade --user
+
+# Add AWS CLI executable to the PATH
+export PATH=$PATH:$HOME/.local/bin
+
+# Configure AWS CLI with your secrets
+aws configure set aws_access_key_id ${{ secrets.AWS_ACCESS_KEY_ID }}
+aws configure set aws_secret_access_key ${{ secrets.AWS_SECRET_ACCESS_KEY }}
+aws configure set default.region us-east-1
+
 # Specify your S3 bucket name and CloudFormation stack prefix
 S3_BUCKET_NAME="havamal-talks-serverless"
 STACK_PREFIX="havamal-talks-serverless"
